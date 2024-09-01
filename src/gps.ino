d296fa664400e93315c420ea56da838f95943337
adahmane <ahcene.dahmane@trialog.com>
2024-09-01 14:12:43 +0200

    feat: sending gps data via hc06 bluetooth module to the MIT AppInventor apk

--- src/gps.ino
+++ src/gps.ino
@@ -2,16 +2,22 @@
 #include <TinyGPS++.h>
 
 // The serial connection to the GPS module
-SoftwareSerial ss(3, 4); // RX, TX
+SoftwareSerial ss(3, 4); // RX, TX for GPS
 TinyGPSPlus gps;
+
+// The serial connection to the Bluetooth HC-06 module
+SoftwareSerial btSerial(7, 8); // RX, TX for Bluetooth
 
 double prevLat = 0.0, prevLng = 0.0; // Previous location coordinates
 double totalDistance = 0.0; // Total distance traveled
 
 void setup() {
-  Serial.begin(9600);
-  ss.begin(9600);
+  Serial.begin(9600);       // Start the serial monitor
+  ss.begin(9600);           // Start the GPS module
+  btSerial.begin(9600);     // Start the Bluetooth module
+  
   Serial.println("GPS Module Initialized");
+  Serial.println("Bluetooth Module Initialized");
 }
 
 void loop() {
@@ -20,21 +26,35 @@ void loop() {
     gps.encode(ss.read());
     
     if (gps.location.isUpdated()) {
-      // Display Latitude and Longitude
+      // Display Latitude and Longitude on the Serial Monitor
       Serial.print("Latitude: ");
       Serial.println(gps.location.lat(), 6);
       Serial.print("Longitude: ");
       Serial.println(gps.location.lng(), 6);
       
-      // Display Date
+      // Send Latitude and Longitude to the Bluetooth App
+      btSerial.print("Lat: ");
+      btSerial.println(gps.location.lat(), 6);
+      btSerial.print("Lng: ");
+      btSerial.println(gps.location.lng(), 6);
+      
+      // Display Date on the Serial Monitor
       Serial.print("Date: ");
       Serial.print(gps.date.month());
       Serial.print("/");
       Serial.print(gps.date.day());
       Serial.print("/");
       Serial.println(gps.date.year());
+      
+      // Send Date to the Bluetooth App
+      btSerial.print("Date: ");
+      btSerial.print(gps.date.month());
+      btSerial.print("/");
+      btSerial.print(gps.date.day());
+      btSerial.print("/");
+      btSerial.println(gps.date.year());
 
-      // Display Time in UTC
+      // Display Time in UTC on the Serial Monitor
       Serial.print("Time (UTC): ");
       if (gps.time.hour() < 10) Serial.print(F("0"));
       Serial.print(gps.time.hour());
@@ -45,9 +65,24 @@ void loop() {
       if (gps.time.second() < 10) Serial.print(F("0"));
       Serial.println(gps.time.second());
 
-      // Calculate speed (km/h)
+      // Send Time to the Bluetooth App
+      btSerial.print("Time (UTC): ");
+      if (gps.time.hour() < 10) btSerial.print(F("0"));
+      btSerial.print(gps.time.hour());
+      btSerial.print(":");
+      if (gps.time.minute() < 10) btSerial.print(F("0"));
+      btSerial.print(gps.time.minute());
+      btSerial.print(":");
+      if (gps.time.second() < 10) btSerial.print(F("0"));
+      btSerial.println(gps.time.second());
+
+      // Display Speed (km/h) on the Serial Monitor
       Serial.print("Speed (km/h): ");
       Serial.println(gps.speed.kmph());
+
+      // Send Speed to the Bluetooth App
+      btSerial.print("Speed (km/h): ");
+      btSerial.println(gps.speed.kmph());
 
       // Calculate distance traveled since last update
       if (prevLat != 0.0 && prevLng != 0.0) {
@@ -59,17 +94,26 @@ void loop() {
         
         Serial.print("Distance from last point (m): ");
         Serial.println(distance);
+
+        // Send Distance to the Bluetooth App
+        btSerial.print("Distance from last point (m): ");
+        btSerial.println(distance);
       }
 
       // Update previous coordinates
       prevLat = gps.location.lat();
       prevLng = gps.location.lng();
       
-      // Display total distance
+      // Display total distance on the Serial Monitor
       Serial.print("Total Distance Traveled (m): ");
       Serial.println(totalDistance);
+
+      // Send total distance to the Bluetooth App
+      btSerial.print("Total Distance Traveled (m): ");
+      btSerial.println(totalDistance);
       
       Serial.println(); // Blank line for readability
+      btSerial.println(); // Blank line for readability in the app
     }
   }
 }
